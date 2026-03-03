440269ef14d2237a4820345f7502169b84562706
SantiagoCalderon11 <santiagocalderonhe@gmail.com>
2026-03-03 18:43:34 -0500

    Creación del Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import './App.css'
+import Header from "./components/Header";
+import "./App.css";
 
+function App() {
+  return (
+    <>
+      <Header />
 
-export default App
+      <main className="main">
+        <h2>Bienvenida a StudentsRepo</h2>
+        <p>
+          Una plataforma donde los estudiantes pueden compartir y consultar
+          proyectos académicos fácilmente.
+        </p>
+
+      </main>
+    </>
+  );
+}
+
+export default App;
